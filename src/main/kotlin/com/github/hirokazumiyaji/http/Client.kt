422c60b20050324ae75644b5e7f0e65cad7caa27
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2018-01-22 07:31:03 +0900

    Initial setting

---
+++ src/main/kotlin/com/github/hirokazumiyaji/http/Client.kt
@@ -0,0 +1,64 @@
+package com.github.hirokazumiyaji.http
+
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import reactor.core.publisher.Mono
+
+class Client(private val http: OkHttpClient) {
+    fun request(request: Request): Mono<Response> {
+        return Mono.just(http.newCall(request).execute())
+    }
+
+    fun get(url: String, headers: Headers? = null): Mono<Response> {
+        var req = Request.Builder()
+            .url(url)
+            .get()
+        headers?.let {
+            req = req.headers(it)
+        }
+        return request(req.build())
+    }
+
+    fun post(url: String, body: RequestBody, headers: Headers? = null): Mono<Response> {
+        var req = Request.Builder()
+            .url(url)
+            .post(body)
+        headers?.let {
+            req = req.headers(it)
+        }
+        return request(req.build())
+    }
+
+    fun put(url: String, body: RequestBody, headers: Headers? = null): Mono<Response> {
+        var req = Request.Builder()
+            .url(url)
+            .put(body)
+        headers?.let {
+            req = req.headers(it)
+        }
+        return request(req.build())
+    }
+
+    fun patch(url: String, body: RequestBody, headers: Headers? = null): Mono<Response> {
+        var req = Request.Builder()
+            .url(url)
+            .patch(body)
+        headers?.let {
+            req = req.headers(it)
+        }
+        return request(req.build())
+    }
+
+    fun delete(url: String, body: RequestBody? = null, headers: Headers? = null): Mono<Response> {
+        var req = Request.Builder()
+            .url("")
+            .delete(body)
+        headers?.let {
+            req = req.headers(it)
+        }
+        return request(req.build())
+    }
+}
---
+++ src/test/kotlin/com/github/hirokazumiyaji/http/ClientTest.kt
@@ -0,0 +1,33 @@
+package com.github.hirokazumiyaji.http
+
+import okhttp3.OkHttpClient
+import org.junit.Test
+
+class ClientTest {
+
+    private val client = Client(OkHttpClient())
+
+    @Test
+    fun testGet() {
+    }
+
+    @Test
+    fun testPost() {
+
+    }
+
+    @Test
+    fun testPut() {
+
+    }
+
+    @Test
+    fun testPatch() {
+
+    }
+
+    @Test
+    fun testDelete() {
+
+    }
+}
